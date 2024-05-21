7379f3ef2cfc85b58b9ba03cc8de774f41f4159f
YELDAMUMESH <gliscor68@gmail.com>
2024-05-21 19:26:00 +0530

    added deletion

---
+++ array operations/deletion.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int i;
+void display(int arr[],int n)
+{
+    
+    for(i=0;i<n;i++)
+    {
+        printf("%d",arr[i]);
+
+    }
+    printf("\n");
+}
+
+
+void delete(int arr[],int size,int index)
+{
+    for(i=index;i<size-1;i++)
+    {
+        arr[i]=arr[i+1];
+
+    }
+
+
+}
+
+
+int main()
+{
+int arr[]={0,1,2,3,4};
+int size=5;
+int index=0;
+display(arr,size);
+delete( arr,size,index);
+size=size-1;
+display(arr,size);
+return 0;
+}
